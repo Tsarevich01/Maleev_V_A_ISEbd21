e83563802ef96b52413776abf7acf4e8006c20f4
Tsarevich01 <pytinvovan@mail.ru>
2018-11-29 20:03:10 +0400

    la1

--- Maleev_V_A_ISEbd21/Form1.cs
+++ Maleev_V_A_ISEbd21/Form1.cs
@@ -12,9 +12,61 @@ namespace Maleev_V_A_ISEbd21
 {
     public partial class Form1 : Form
     {
+
+        private Benzovoz car;
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxCars.Width, pictureBoxCars.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            car.DrawTruck(gr);
+            pictureBoxCars.Image = bmp;
+        }
+        /// <summary>
+        /// Обработка нажатия кнопки "Создать"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            car = new Benzovoz(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue,
+           Color.Yellow);
+            car.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxCars.Width,
+           pictureBoxCars.Height);
+            Draw();
+        }
+        /// <summary>
+        /// Обработка нажатия кнопок управления
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            //получаем имя кнопки
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    car.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    car.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    car.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    car.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
     }
 }
