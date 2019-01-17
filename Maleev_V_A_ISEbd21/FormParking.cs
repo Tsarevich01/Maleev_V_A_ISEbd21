0bfebb7a427cde998fdc82a1bea728ac90ef9bb4
Tsarevich01 <pytinvovan@mail.ru>
2019-01-17 17:18:15 +0400

    lab4

--- Maleev_V_A_ISEbd21/FormParking.cs
+++ Maleev_V_A_ISEbd21/FormParking.cs
@@ -12,23 +12,34 @@ namespace Maleev_V_A_ISEbd21
 {
     public partial class FormParking : Form
     {
-        Parking<Itest> parking;
+        MultiLevelParking parking;
+        
+        private const int countLevel = 5;
         public FormParking()
         {
             InitializeComponent();
-            parking = new Parking<Itest>(12, pictureBoxParking.Width,
-           pictureBoxParking.Height);
+            parking = new MultiLevelParking(countLevel, pictureBoxParking.Width,
+pictureBoxParking.Height);
             Draw();
+            for (int i = 0; i < countLevel; i++)
+            {
+                listBoxlevel.Items.Add("Уровень " + (i + 1));
+            }
+            listBoxlevel.SelectedIndex = 0;
         }
         /// <summary>
         /// Метод отрисовки парковки
         /// </summary>
         private void Draw()
         {
-            Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
-            Graphics gr = Graphics.FromImage(bmp);
-            parking.Draw(gr);
-            pictureBoxParking.Image = bmp;
+            if (listBoxlevel.SelectedIndex > -1)
+            {
+                Bitmap bmp = new Bitmap(pictureBoxParking.Width,
+pictureBoxParking.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                parking[listBoxlevel.SelectedIndex].Draw(gr);
+                pictureBoxParking.Image = bmp;
+            }
         }
         /// <summary>
         /// Обработка нажатия кнопки "Припарковать автомобиль"
@@ -63,53 +74,78 @@ namespace Maleev_V_A_ISEbd21
 
         private void buttonBenzovoz_Click(object sender, EventArgs e)
         {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (listBoxlevel.SelectedIndex > -1)
             {
-                ColorDialog dialogDop = new ColorDialog();
-                if (dialogDop.ShowDialog() == DialogResult.OK)
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    var car = new Benzovoz(100, 1000, dialog.Color, dialogDop.Color);
-                    int place = parking + car;
-                    Draw();
+                    ColorDialog dialogDop = new ColorDialog();
+                    if (dialogDop.ShowDialog() == DialogResult.OK)
+                    {
+                        var car = new Benzovoz(100, 1000, dialog.Color, dialogDop.Color);
+                        int place = parking[listBoxlevel.SelectedIndex] + car;
+                        if (place == -1)
+                        {
+                            MessageBox.Show("Нет свободных мест", "Ошибка",
+                           MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        Draw();
+                    }
                 }
             }
         }
 
         private void buttonTruck_Click(object sender, EventArgs e)
         {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (listBoxlevel.SelectedIndex > -1)
             {
-                var car = new Truck(100, 1000, dialog.Color);
-                int place = parking + car;
-                Draw();
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var car = new Truck(100, 1000, dialog.Color);
+                    int place = parking[listBoxlevel.SelectedIndex] + car;
+                    if (place == -1)
+                    {
+                        MessageBox.Show("Нет свободных мест", "Ошибка",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    Draw();
+                }
             }
         }
 
         private void buttonTake_Click(object sender, EventArgs e)
         {
-            if (maskedTextBox.Text != "")
+            if (listBoxlevel.SelectedIndex > -1)
             {
-                var car = parking - Convert.ToInt32(maskedTextBox.Text);
-                if (car != null)
+                if (maskedTextBox.Text != "")
                 {
-                    Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width,
-                   pictureBoxTakeCar.Height);
-                    Graphics gr = Graphics.FromImage(bmp);
-                    car.SetPosition(5, 5, pictureBoxTakeCar.Width,
-                   pictureBoxTakeCar.Height);
-                    car.DrawTruck(gr);
-                    pictureBoxTakeCar.Image = bmp;
+                    var car = parking[listBoxlevel.SelectedIndex] -
+                   Convert.ToInt32(maskedTextBox.Text);
+                    if (car != null)
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width,
+                       pictureBoxTakeCar.Height);
+                        Graphics gr = Graphics.FromImage(bmp);
+                        car.SetPosition(5, 5, pictureBoxTakeCar.Width,
+                       pictureBoxTakeCar.Height);
+                        car.DrawTruck(gr);
+                        pictureBoxTakeCar.Image = bmp;
+                    }
+                    else
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width,
+                       pictureBoxTakeCar.Height);
+                        pictureBoxTakeCar.Image = bmp;
+                    }
+                    Draw();
                 }
-                else
-                {
-                    Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width,
-                   pictureBoxTakeCar.Height);
-                    pictureBoxTakeCar.Image = bmp;
-                }
-                Draw();
+
             }
+        }
+        private void listBoxlevel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Draw();
         }
     }
 }
---
+++ Maleev_V_A_ISEbd21/MultiLevelParking.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maleev_V_A_ISEbd21
+{
+    public class MultiLevelParking
+    {
+        /// <summary>
+        /// Список с уровнями парковки
+        /// </summary>
+        List<Parking<Itest>> parkingStages;
+        /// <summary>
+        /// Сколько мест на каждом уровне
+        /// </summary>
+        private const int countPlaces = 20;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="countStages">Количество уровенй парковки</param>
+        /// <param name="pictureWidth"></param>
+        /// <param name="pictureHeight"></param>
+        public MultiLevelParking(int countStages, int pictureWidth, int pictureHeight)
+        {
+            parkingStages = new List<Parking<Itest>>();
+            for (int i = 0; i < countStages; ++i)
+            {
+                parkingStages.Add(new Parking<Itest>(countPlaces, pictureWidth,
+               pictureHeight));
+            }
+        }
+        /// <summary>
+        /// Индексатор
+        /// </summary>
+        /// <param name="ind"></param>
+        /// <returns></returns>
+        public Parking<Itest> this[int ind]
+        {
+            get
+            {
+                if (ind > -1 && ind < parkingStages.Count)
+                {
+                    return parkingStages[ind];
+                }
+                return null;
+            }
+        }
+    }
+}
